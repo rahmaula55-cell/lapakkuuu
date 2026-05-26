653654aa672b1afd051c6229919d3630b114cbcf
Mmanusia <mmanusia987@gmail.com>
2026-05-26 14:18:53 +0700

    profile

--- lib/services/pencarianmenuser_rhma.dart
+++ lib/services/pencarianmenuser_rhma.dart
@@ -6,6 +6,6 @@ List<PencarianmnumodelRhma> penn = [
     imageUrl: "assets/image/skincare.jpg",
     title: "skincare",
   ),
-  PencarianmnumodelRhma(imageUrl: "assets/image/puma.jpg", title: "Sepatu"),
+  PencarianmnumodelRhma(imageUrl: "assets/image/puma.jpg", title: "Bucket"),
   PencarianmnumodelRhma(imageUrl: "assets/image/puma.jpg", title: "Sepatu"),
 ];
